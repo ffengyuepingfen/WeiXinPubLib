3f57ed8295eaf55b5a12320e53bcbea65a99f09b
王相博 <laoyouhub@icloud.com>
2026-03-16 17:24:09 +0800

    更新GitHub 配置

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Gitee Pages 部署配置
-  // 如果部署到 https://用户名.gitee.io/WeiXinPubLib/，需要设置 base
-  // 如果部署到根目录 https://用户名.gitee.io/，设置为 '/'
+  // GitHub Pages 部署配置
+  // 部署到 https://用户名.github.io/WeiXinPubLib/
   base: '/WeiXinPubLib/',
   build: {
     outDir: 'dist',
